9027b4a2715cf69efd713f4e691d30ba30a134b7
Ray <duruyoungcr@gmail.com>
2020-05-22 23:17:32 +0100

    Updates

---
+++ precache-manifest.54f075b017e38f01301ff6066a6f0d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bcb0eb758d42b249f6e95b01774782",
+    "url": "/contactsmanager/index.html"
+  },
+  {
+    "revision": "bd2f41f395b4a0b41975",
+    "url": "/contactsmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3b8f722a669d4f63fea3",
+    "url": "/contactsmanager/static/css/main.25fe0e90.chunk.css"
+  },
+  {
+    "revision": "bd2f41f395b4a0b41975",
+    "url": "/contactsmanager/static/js/2.291c1b9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactsmanager/static/js/2.291c1b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8f722a669d4f63fea3",
+    "url": "/contactsmanager/static/js/main.c0a00b7f.chunk.js"
+  },
+  {
+    "revision": "18d00917586f22f6f021",
+    "url": "/contactsmanager/static/js/runtime-main.b66db609.js"
+  }
+]);
