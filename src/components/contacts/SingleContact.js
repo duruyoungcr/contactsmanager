e11bea217c6189147d7b2678290006d0461f3dd6
Ray <duruyoungcr@gmail.com>
2020-05-23 14:22:14 +0100

    added SingleContact.js

---
+++ src/components/contacts/SingleContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class SingleContact extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+export default SingleContact;
